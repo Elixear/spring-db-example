3ac5ae50fedb2e529ab34cc17644e2ca8a10fbb9
Imad-Eddine Izouakane <imadeddine.izouakane@berexia.com>
2020-04-11 13:59:31 +0100

    Added test

---
+++ src/main/java/com/example/BookService.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    public int count() {
+        return 10;
+    }
+}
--- src/test/java/com/example/SpringDbExampleApplicationTests.java
+++ src/test/java/com/example/SpringDbExampleApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringDbExampleApplicationTests {
+	@Autowired
+	private BookService bookService;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void validate() {
+		assertEquals(10, bookService.count(), "Yay");
+	}
 }
